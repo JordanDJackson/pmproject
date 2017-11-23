0e6a99c8342f77f5fb25bd4ce5bc70a507ed6bce
Jordan Jackson <jordandjackson21@gmail.com>
2017-11-23 11:01:55 -0800

    Prevent dup usernames

--- routes/users.js
+++ routes/users.js
@@ -27,42 +27,58 @@ router.post('/register', function (req, res) {
   req.checkBody('password', 'Password is required').notEmpty()
   req.checkBody('password2', 'Passwords do not match').equals(req.body.password)
 
-  let errors = req.validationErrors()
-  if (errors) {
-      // if there are any errors re-render the register page
-    res.render('register', {
-      errors: errors
-    })
-  } else {
-  // create a mongoose iteration and save the stuff the user put in to the database
-    let newUser = new User({
-      name: name,
-      email: email,
-      username: username,
-      password: password
-    })
-  // bcrypt hashing its the password with random salt
-    bcrypt.genSalt(10, function (err, salt) {
-      bcrypt.hash(newUser.password, salt, function (err, hash) {
-        if (err) {
-          console.log(err)
-        }
-      // make user hashed password equal to users actual password
-        newUser.password = hash
-      // save mongoose user iteration with hashed password to mongo
-        newUser.save(function (err) {
-          if (err) {
-            console.log(err)
+  // check to make this user hasnt registered
+  var testThisName = username;
+  var queryforUsername = {};
+  queryforUsername["username"] = testThisName;
+  User.find(queryforUsername, function(error,item) {
+    if(error) throw error;
+    if(item.length > 0) {
+      req.flash('warning', 'That username is taken!')
+      console.log(item);
+      console.log(item.length);
+    // then redirect to user logged in page
+      res.redirect('/users/register');
+    } else if (item.length == 0) {
+          console.log("Creating an account with the free username");
+          let errors = req.validationErrors()
+          if (errors) {
+              // if there are any errors re-render the register page
+            res.render('register', {
+              errors: errors
+            })
           } else {
-          // flash message to let the user know they are logged in
-            req.flash('success', 'You are now registered and can log in!')
-          // then redirect to user logged in page
-            res.redirect('/users/login')
+          // create a mongoose iteration and save the stuff the user put in to the database
+            let newUser = new User({
+              name: name,
+              email: email,
+              username: username,
+              password: password
+            })
+          // bcrypt hashing its the password with random salt
+            bcrypt.genSalt(10, function (err, salt) {
+              bcrypt.hash(newUser.password, salt, function (err, hash) {
+                if (err) {
+                  console.log(err)
+                }
+              // make user hashed password equal to users actual password
+                newUser.password = hash
+              // save mongoose user iteration with hashed password to mongo
+                newUser.save(function (err) {
+                  if (err) {
+                    console.log(err)
+                  } else {
+                  // flash message to let the user know they are logged in
+                    req.flash('success', 'You are now registered and can log in!')
+                  // then redirect to user logged in page
+                    res.redirect('/users/login')
+                  }
+                })
+              })
+            })
           }
-        })
-      })
-    })
-  }
+        }
+  })
 })
 // renders the login form
 router.get('/login', function (req, res) {
