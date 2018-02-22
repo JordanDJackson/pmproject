57aa581557e51bdf8498e09e4ebee22cf1b88ab8
Jordan Jackson <jordandjackson21@gmail.com>
2018-02-22 13:26:50 -0800

    general fixes

--- algos/desvia.js
+++ algos/desvia.js
@@ -199,7 +199,7 @@ module.exports = function desvia(q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,
           break;
         case 'Yes':
           perfectScore -= 5;
-          faults["Problems has many problems within it"] = -5;
+          faults["Problem has many problems within it"] = -5;
           break;
         default:
           break;
